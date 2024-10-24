b16ca94f0980bb34f8b4e761b16f2dc871838ab8
Bojken07 <bojken.zaimi@stud.sti.se>
2024-10-24 15:11:39 +0200

    skapa anslutning med github

---
+++ src/main/java/com/bojken/enhetsomvandlare_baking_mattlagning/EnhetsomvandlareBakingMattlagningApplication.java
@@ -0,0 +1,13 @@
+package com.bojken.enhetsomvandlare_baking_mattlagning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnhetsomvandlareBakingMattlagningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnhetsomvandlareBakingMattlagningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bojken/enhetsomvandlare_baking_mattlagning/EnhetsomvandlareBakingMattlagningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bojken.enhetsomvandlare_baking_mattlagning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhetsomvandlareBakingMattlagningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

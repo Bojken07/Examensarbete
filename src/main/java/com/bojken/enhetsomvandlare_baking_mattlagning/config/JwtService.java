6b1faf8474279116b77032348cd65209c6757a3e
Bojken07 <bojken.zaimi@stud.sti.se>
2024-10-28 14:24:01 +0100

    Skapade JwtService-klass och definierade SECRET_KEY

---
+++ src/main/java/com/bojken/enhetsomvandlare_baking_mattlagning/config/JwtService.java
@@ -0,0 +1,14 @@
+package com.bojken.enhetsomvandlare_baking_mattlagning.config;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "77397A24432646294A404E635266556A586E327235753778214125442A472D4B";
+
+}

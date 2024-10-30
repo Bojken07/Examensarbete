28b31b20c6bbd52ad4b82a77216451a682abc3b9
Bojken07 <bojken.zaimi@stud.sti.se>
2024-10-30 16:55:17 +0100

    Implementera token-generering med extra claims i JwtService

--- src/main/java/com/bojken/enhetsomvandlare_baking_mattlagning/config/JwtService.java
+++ src/main/java/com/bojken/enhetsomvandlare_baking_mattlagning/config/JwtService.java
@@ -3,6 +3,7 @@ package com.bojken.enhetsomvandlare_baking_mattlagning.config;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,5 +11,23 @@ import java.util.Map;
 public class JwtService {
 
     private static final String SECRET_KEY = "77397A24432646294A404E635266556A586E327235753778214125442A472D4B";
-
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 }

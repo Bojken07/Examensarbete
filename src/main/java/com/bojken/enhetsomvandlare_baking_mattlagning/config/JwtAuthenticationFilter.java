cc3263c3f48f32b3d6cdebd08fa2a3592d6e7365
Bojken07 <bojken.zaimi@stud.sti.se>
2024-11-05 19:20:28 +0100

    Klassdeklaration JwtAuthentikationFilter och Deklarera dependencies JwtService och UserDetailsService

---
+++ src/main/java/com/bojken/enhetsomvandlare_baking_mattlagning/config/JwtAuthenticationFilter.java
@@ -0,0 +1,14 @@
+package com.bojken.enhetsomvandlare_baking_mattlagning.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+}

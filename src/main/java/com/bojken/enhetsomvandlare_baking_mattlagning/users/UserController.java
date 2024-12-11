e26336f6ba7a1d541f141255f3a464619a2d72d7
Bojken07 <bojken.zaimi@stud.sti.se>
2024-12-11 11:10:13 +0100

    Add: Hantera användarens uppdatering

--- src/main/java/com/bojken/enhetsomvandlare_baking_mattlagning/users/UserController.java
+++ src/main/java/com/bojken/enhetsomvandlare_baking_mattlagning/users/UserController.java
@@ -1,9 +1,8 @@
 package com.bojken.enhetsomvandlare_baking_mattlagning.users;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -14,5 +13,10 @@ public class UserController {
     @DeleteMapping("/delete_user/{id}")
     private void deleteUserById(@PathVariable("id") long id) {
         this.userService = userService;
+    }
+
+    @PutMapping("/update_user/{id}")
+    private ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+        return userService.update(user.getUsername(), user.getRole(), id);
     }
 }

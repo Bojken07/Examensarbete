d0b57d7ff7d1ce2fc2d2f6dfbb343c58bd94691c
Bojken07 <bojken.zaimi@stud.sti.se>
2024-12-13 09:38:24 +0100

    Add:Skapa Unit klass: Implementera en JPA-annoterad entitetsklass och lägg relevanta klass

--- src/main/java/com/bojken/enhetsomvandlare_baking_mattlagning/units/UnitRepository.java
+++ src/main/java/com/bojken/enhetsomvandlare_baking_mattlagning/units/UnitRepository.java
@@ -3,8 +3,10 @@ package com.bojken.enhetsomvandlare_baking_mattlagning.units;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface UnitRepository extends JpaRepository <Unit, Long> {
 
     @Query("SELECT u FROM Unit u WHERE u.unitName = ?1")
-    Optional<Unit> existByName(String unitName);
+    Optional<Unit> existsByName(String unitName);
 }

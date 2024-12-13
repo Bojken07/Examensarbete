d1a230f4a897af2f9c47a8f3e6e4c9c8d9c133b1
Bojken07 <bojken.zaimi@stud.sti.se>
2024-12-13 11:21:30 +0100

    Add: Skapa UserService klass: Skapa en serviceklass som interagerar med UserRepository

---
+++ src/main/java/com/bojken/enhetsomvandlare_baking_mattlagning/users/UserService.java
@@ -0,0 +1,14 @@
+package com.bojken.enhetsomvandlare_baking_mattlagning.users;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+}
